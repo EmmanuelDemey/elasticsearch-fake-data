d65f187294aad07dfd4801b76ea10d1bfacd628c
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-01-21 19:53:29 +0100

    feat: generate random data for integer and strings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-module.exports = () => true;
+const { faker } = require("@faker-js/faker");
+
+module.exports = (index) => {
+  const mappings = index.mappings;
+  const properties = mappings.properties;
+
+  const generated = Object.entries(properties).reduce((acc, [propertyName, configuration]) => {
+    if (configuration.type === "integer") {
+      return {
+        ...acc,
+        [propertyName]: faker.datatype.number(),
+      };
+    }
+    return {
+      ...acc,
+      [propertyName]: faker.datatype.string(),
+    };
+  }, {});
+
+  return [generated];
+};
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,23 @@
-const main = require('./index');
+const generate = require("./index");
 
-describe('main', () => {
-    it('should return true', () => {
-        expect(main()).toBeTruthy()
-    })
-})
+describe("main", () => {
+  it("should return true", () => {
+    expect(
+      generate({
+        mappings: {
+          properties: {
+            age: { type: "integer" },
+            email: { type: "keyword" },
+            name: { type: "text" },
+          },
+        },
+      })
+    ).toEqual([
+      {
+        age: expect.any(Number),
+        email: expect.any(String),
+        name: expect.any(String),
+      },
+    ]);
+  });
+});
