b0c2b11054f3bf4afabaa346c58161e77ed36cac
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-02-09 13:39:22 +0100

    feat: manage object

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 const { faker } = require("@faker-js/faker");
 
-module.exports = (index) => {
-  const mappings = index.mappings;
+const generateData = (mappings) => {
   const properties = mappings.properties;
 
-  const generated = Object.entries(properties).reduce((acc, [propertyName, configuration]) => {
-    if (configuration.type === "integer") {
+  return Object.entries(properties).reduce((acc, [propertyName, configuration]) => {
+
+    if(configuration.properties){
+      return {
+        ...acc,
+        [propertyName]: generateData(configuration)
+      }
+    }
+    if (["long", "integer", "short", "byte", "double", "half_float", "scaled_float", "unsigned_long"].includes(configuration.type)) {
       return {
         ...acc,
         [propertyName]: faker.datatype.number(),
       };
     }
+
+    if (["float"].includes(configuration.type)) {
+      return {
+        ...acc,
+        [propertyName]: faker.datatype.float(),
+      };
+    }
+
     return {
       ...acc,
       [propertyName]: faker.datatype.string(),
     };
   }, {});
+}
+module.exports = (index) => {
+  const mappings = index.mappings;
+  
+  const generated = generateData(mappings)
 
   return [generated];
 };
